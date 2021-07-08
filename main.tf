0ef9fd06b7125099c0012e3384d56c9e9597e721
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-07 23:56:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -126,9 +126,9 @@ resource "aws_route53_record" "www" {
   name    = "${var.www_domain_name}"
   type    = "A"
 
-  alias = {
-    name                   = "${aws_cloudfront_distribution.www_distribution.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.www_distribution.hosted_zone_id}"
+  alias {
+    name = "${aws_cloudfront_distribution.www_distribution.domain_name}"
+    zone_id  = "${aws_cloudfront_distribution.www_distribution.hosted_zone_id}"
     evaluate_target_health = false
   }
 }
