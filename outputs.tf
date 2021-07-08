55b479c251d49749233de03eeefdf39e7c3fd80d
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-07 22:02:19 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "s3_bucket_details" {
   value       = aws_s3_bucket.www
   description = "The details of the S3 bucket"
 }
+
+output "aws_cloudfront_details" {
+  value       = aws_cloudfront_distribution.www_distribution
+  description = "The details of the cloudfront"
+}
